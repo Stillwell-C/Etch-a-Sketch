7aa241ef13a3b16d41217e1c2c718457c20d1b62
Stillwell-C <danceswithhodge@gmail.com>
2022-03-16 12:33:44 +0900

    Change cell text

--- javascript.js
+++ javascript.js
@@ -2,7 +2,6 @@ const container = document.querySelector('#container')
 
 const gridDiv = document.createElement('div');
 gridDiv.classList.add('gridDiv');
-gridDiv.textContent = "gridDiv";
 container.appendChild(gridDiv);
 
 let cellCount = 256;
@@ -11,7 +10,7 @@ function createCells(c) {
     for(i = 0; i < c; i++) {
     let cell = document.createElement('div');
     cell.classList.add('cell');
-    cell.textContent = `${1+i}`;
+    cell.textContent = 'cell';
     gridDiv.appendChild(cell);
     }
 }
