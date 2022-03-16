79b9506c306ab3ea0220f60e7c46df5f6abd66f2
Stillwell-C <danceswithhodge@gmail.com>
2022-03-16 14:55:52 +0900

    Add event listener to cells

--- javascript.js
+++ javascript.js
@@ -7,12 +7,25 @@ container.appendChild(gridDiv);
 let cellCount = 256;
 
 function createCells(c) {
-    for(i = 0; i < c; i++) {
+    for(let i = 0; i < c; i++) {
     let cell = document.createElement('div');
     cell.classList.add('cell');
-    cell.textContent = 'cell';
+    // cell.textContent = 'cell';
     gridDiv.appendChild(cell);
     }
 }
 
 createCells(cellCount);
+
+
+let cells = document.querySelectorAll('.cell').forEach(item => {
+    item.addEventListener('mouseover', () => {
+        console.log("mouseover");
+    })
+})
+// for (let i = 0; i < cellCount; i++) {
+//     cells.addEventListener('mouseover', () => {
+//         console.log("mouseover");
+//     });
+// }
+
